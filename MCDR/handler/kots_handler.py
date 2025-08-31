b2719fdab99f3890fb3be67b9c26463c6d911a11
Polaris_Light <995905922@qq.com>
2025-08-31 10:54:45 +0800

    fix(MCDR): fix handler

--- MCDR/handler/kots_handler.py
+++ MCDR/handler/kots_handler.py
@@ -21,7 +21,7 @@ class KOTSHandler(ForgeHandler):
 	@override
 	def parse_player_left(self, info):
 		if not info.is_user:
-			if (m := re.compile(r'(?P<name>[^ ]+) left the game').fullmatch(info.content)) is not None:
+			if (m := re.compile(r'(?P<name>.+) left the game').fullmatch(info.content)) is not None:
 				if self._verify_player_name(m['name'].split(" ", 1)[0]):
 					return m['name']
 		return None
@@ -29,4 +29,6 @@ class KOTSHandler(ForgeHandler):
 if __name__ == '__main__':
 	handler = KOTSHandler()
 	test = handler.parse_server_stdout('[01:02:03] [Server thread/INFO]: <PlayerName (虚空)> Message')
+	test2 = handler.parse_player_left(handler.parse_server_stdout('[10:43:09] [Server thread/INFO] [minecraft/MinecraftServer]: Polaris_Light (虚空) left the game'))
 	print(test.player)
+	print(test2)
