e961f3e56b4e0a162f6f4fe7cd17d7de923e3c12
Polaris_Light <995905922@qq.com>
2025-11-04 11:10:31 +0800

    chore(structure): move to pakku

--- server-installer/install.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-java -jar forge-1.20.1-47.3.7-installer.jar --installServer
-mrpack-install-windows.exe KumoOTagayasu.mrpack --server-file ./forge-1.20.1-47.3.7-installer.jar --server-dir ./
-@echo Installing is done,Now you can exit this window and run server by run.bat.
-pause
--- server-installer/install.sh
+++
@@ -1,2 +0,0 @@
-java -jar forge-1.20.1-47.3.7-installer.jar --installServer
-mrpack-install-linux KumoOTagayasu.mrpack --server-file ./forge-1.20.1-47.3.7-installer.jar --server-dir ./
